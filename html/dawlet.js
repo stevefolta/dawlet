ca6109e2a130b0976f42268dbd028bc0f399673c
Steve Folta <steve@folta.net>
2015-07-21 14:27:22 -0700

    Interface popup.
    You can switch interfaces in the middle of playback!

--- html/dawlet.js
+++ html/dawlet.js
@@ -9,6 +9,22 @@ function log(message) {
 	}
 
 
+function populate_interfaces(interfaces) {
+	// Clear out existing elements.
+	var popup = document.getElementById("interface-popup");
+	while (popup.length > 0)
+		popup.remove(0);
+
+	// Add the interfaces.
+	interfaces.forEach(function(interface) {
+		var option = document.createElement('option');
+		option.value = interface;
+		option.text = interface;
+		popup.add(option);
+		});
+	}
+
+
 function load() {
 	log("Loaded page.");
 
@@ -27,15 +43,17 @@ function load() {
 		};
 	document.getElementById("play").onclick = function() {
 		websocket.send("play");
-		}
+		};
 	document.getElementById("stop").onclick = function() {
 		websocket.send("stop");
-		}
+		};
 	document.getElementById("go").onclick = function() {
 		websocket.send("open-project \"test/test.json\"");
-		websocket.send("list-interfaces");
 		websocket.send("play");
-		}
+		};
+	document.getElementById("interface-popup").onchange = function(event) {
+		websocket.send("select-interface \"" + event.target.value + "\"");
+		};
 	window.setInterval(function() {
 		websocket.send("get-play-head");
 		},
@@ -48,8 +66,10 @@ function load() {
 		if (event.data.startsWith("interfaces ")) {
 			var json = event.data.substr(10);
 			var interfaces = JSON.parse(json);
-			if (interfaces && interfaces[0])
+			if (interfaces && interfaces[0]) {
+				populate_interfaces(interfaces);
 				websocket.send("select-interface \"" + interfaces[0] + "\"");
+				}
 			}
 		else if (event.data.startsWith("play-head ")) {
 			var play_head = parseFloat(event.data.substr(10));
@@ -59,6 +79,7 @@ function load() {
 	websocket.onopen = function (event) {
 		try {
 			websocket.send("ping");
+			websocket.send("list-interfaces");
 			websocket.send("get-play-head");
 			}
 		catch (e) {
